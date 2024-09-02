2332d299474d10c146d72ada13e866db0a5863ab
qkrehfud@gmail.com <147295498+DogyeomPark@users.noreply.github.com>
2024-09-02 11:43:49 +0900

    2nd September

--- src/server.js
+++ src/server.js
@@ -1,22 +1,19 @@
 import express from 'express';
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+import userRouter from "./routers/userRouter.js";
+
 
 const PORT = 4000;
 
 const app= express();
+const logger = morgan("dev");
+app.use(logger);
 
-const gossipMiddleware = (req, res, next) => {
-    console.log("I'm in the middle");
-
-
-    res.send("haha")
-    next();
-}
-
-const handleHome = (req, res) => {
-    return res.send("hi");
-};
-
-app.get("/", gossipMiddleware, handleHome);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
     console.log(`Server listening on port http://localhost:${PORT}`);
